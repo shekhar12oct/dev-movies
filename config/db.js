2b8cc2cb8a3d0b35ed4667c8d9757c692753bd7a
Shekhar Dubey <dubeyshekhar1997@gmail.com>
2025-06-19 01:58:49 +0530

    updated err keyword fixed to error

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB Connected');
   } catch (error) {
-    console.log('MongoDB connection Failed', err);
+    console.log('MongoDB connection Failed', error);
     process.exit(1);
   }
 };
